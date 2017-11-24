1dc1eeb4400b094c39a33a39fdeb53d7ce54977b
tianwu <tianwu@huanleguagn.com>
2017-11-24 23:09:41 +0800

    change color

--- src/log.js
+++ src/log.js
@@ -1,3 +1,4 @@
+import cloneDeep from 'lodash.clonedeep';
 import event from './event';
 
 export default function useLog() {
@@ -10,13 +11,13 @@ export default function useLog() {
     });
 
     event.on('Observer', (name) => {
-        console.log('%c %s %c %s', 'color: #666;', 'Observer: ', 'color: #666', name);
+        console.log('%c %s %c %s', 'color: #2196F3;', 'Observer: ', 'color: #666', name);
     });
 
     event.on('set', ({
         target, key, oldValue, value,
     }) => {
-        console.log('%c set: %o %c %s oldValue %o => %c newValue %o', 'color: #ff4a4e;', target, 'color: #666', key, oldValue, 'color: #03A9F4;', value); //eslint-disable-line
+        console.log('%c set: %o %c %s %c oldValue %o => %c newValue %o', 'color: #ff4a4e;', target, 'color: #009688;', key,'color: #263238;', cloneDeep(oldValue), 'color: #03A9F4;', cloneDeep(value)); //eslint-disable-line
     });
 }
 
