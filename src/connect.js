1c495eca8af90fecbf870f7e29717ed4d30ce992
tianwu <tianwu@huanleguagn.com>
2017-11-29 23:47:05 +0800

    处理第一次render没有数据源get 导致数据源一直没变收集

--- src/connect.js
+++ src/connect.js
@@ -1,20 +1,16 @@
 import Observer from './observer';
-import liob from './liob';
 
 const baseRenderKey = Symbol('baseRender');
 const isReCollectDepsKey = Symbol('isReCollectDeps');
-const preObserverKey = Symbol('preObserver');
 const willRender = Symbol('willRender');
 const connectKey = Symbol('connect');
 
 function reactiveRender() {
-    if (this[isReCollectDepsKey]) {
-        const res = this.$observer.collectDeps(this[baseRenderKey].bind(this));
-        liob.currentObserver = this.$observer;
-        this[isReCollectDepsKey] = false;
-        return res;
-    }
-    return this[baseRenderKey]();
+    this[willRender] = true;
+    const res = this.$observer.collectDeps(this[baseRenderKey]);
+    this[isReCollectDepsKey] = false;
+    this[willRender] = false;
+    return res;
 }
 
 function initRender() {
@@ -24,51 +20,15 @@ function initRender() {
             this.forceUpdate();
         }
     }, `${this.constructor.name}.render()`);
-
-    const res = this.$observer.collectDeps(this[baseRenderKey].bind(this));
-    liob.currentObserver = this.$observer;
+    this[isReCollectDepsKey] = true;
     this.render = reactiveRender;
-    return res;
+    return reactiveRender.call(this);
 }
 
 const reactiveMixin = {
     componentWillMount() {
-        if (liob.currentObserver) {
-            this[preObserverKey] = liob.currentObserver;
-        }
-        this[baseRenderKey] = this.render;
+        this[baseRenderKey] = this.render.bind(this);
         this.render = initRender;
-        this[willRender] = true;
-    },
-
-    componentDidMount() {
-        this[willRender] = false;
-        if (this[preObserverKey]) {
-            liob.currentObserver = this[preObserverKey];
-            this[preObserverKey] = null;
-        } else {
-            liob.currentObserver = null;
-        }
-    },
-
-    componentWillUpdate() {
-        this[willRender] = true;
-        if (this[isReCollectDepsKey]) {
-            if (liob.currentObserver) {
-                this[preObserverKey] = liob.currentObserver;
-            }
-            liob.currentObserver = this.$observer;
-        }
-    },
-
-    componentDidUpdate() {
-        this[willRender] = false;
-        if (this[preObserverKey]) {
-            liob.currentObserver = this[preObserverKey];
-            this[preObserverKey] = null;
-        } else {
-            liob.currentObserver = null;
-        }
     },
     componentWillUnmount() {
         this.$observer.unSubscribe();
@@ -82,16 +42,13 @@ function patch(target, funcName, runMixinFirst = false) {
     if (!base) {
         target[funcName] = mixinFunc;
     } else {
-        target[funcName] =
-            runMixinFirst === true
-                ? function funcName(...args) {
-                    mixinFunc.apply(this, args);
-                    base.apply(this, args);
-                }
-                : function funcName(...args) {
-                    base.apply(this, args);
-                    mixinFunc.apply(this, args);
-                };
+        target[funcName] = runMixinFirst === true ? function funcName(...args) {
+            mixinFunc.apply(this, args);
+            base.apply(this, args);
+        } : function funcName(...args) {
+            base.apply(this, args);
+            mixinFunc.apply(this, args);
+        };
     }
 }
 
@@ -99,9 +56,6 @@ export default function connect(target) {
     if (target[connectKey]) return target;
     target[connectKey] = true;
     patch(target.prototype, 'componentWillMount', true);
-    patch(target.prototype, 'componentDidMount');
-    patch(target.prototype, 'componentWillUpdate', true);
-    patch(target.prototype, 'componentDidUpdate', true);
     patch(target.prototype, 'componentWillUnmount');
 
     return target;
