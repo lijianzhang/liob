f44a2ce436f37978041576907ec95b7112de0453
tianwu <tianwu@huanleguagn.com>
2017-12-18 15:04:48 +0800

    test

--- src/react-observer.js
+++ src/react-observer.js
@@ -1,12 +1,19 @@
 import Observer from './observer';
+import liob from './liob';
 
 const baseRenderKey = Symbol('baseRender');
 const isReCollectDepsKey = Symbol('isReCollectDeps');
+const preObserverKey = Symbol('preObserver');
 const connectKey = Symbol('connect');
 
 function reactiveRender() {
     const res = this.$observer.collectDeps(this[baseRenderKey]);
+    liob.currentObserver = this.$observer;
     this[isReCollectDepsKey] = false;
+    if (liob.currentObserver) {
+        this[preObserverKey] = liob.currentObserver;
+    }
+    liob.currentObserver = this.$observer;
     return res;
 }
 
@@ -28,6 +35,23 @@ const reactiveMixin = {
         this.render = initRender;
     },
 
+    componentDidMount() {
+        if (this[preObserverKey]) {
+            liob.currentObserver = this[preObserverKey];
+            this[preObserverKey] = null;
+        } else {
+            liob.currentObserver = null;
+        }
+    },
+
+    componentDidUpdate() {
+        if (this[preObserverKey]) {
+            liob.currentObserver = this[preObserverKey];
+            this[preObserverKey] = null;
+        } else {
+            liob.currentObserver = null;
+        }
+    },
     componentWillUnmount() {
         this.$observer.unSubscribe();
         this.$observer = null;
@@ -41,22 +65,24 @@ function patch(target, funcName, runMixinFirst = false) {
         target[funcName] = mixinFunc;
     } else {
         target[funcName] =
-            runMixinFirst === true ?
-                function funcName(...args) {
+            runMixinFirst === true
+                ? function funcName(...args) {
                     mixinFunc.apply(this, args);
                     base.apply(this, args);
-                } :
-                function funcName(...args) {
+                }
+                : function funcName(...args) {
                     base.apply(this, args);
                     mixinFunc.apply(this, args);
                 };
     }
 }
 
-export default function ReactObserver(target) {
+export default function reactObserver(target) {
     if (target[connectKey]) return target;
     target[connectKey] = true;
     patch(target.prototype, 'componentWillMount', true);
+    patch(target.prototype, 'componentDidMount');
+    patch(target.prototype, 'componentDidUpdate', true);
     patch(target.prototype, 'componentWillUnmount');
 
     return target;
