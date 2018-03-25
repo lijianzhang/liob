d959d5223d2d95dc82ee84945125c3d380467c0d
lijianzhang <coder_jz@163.com>
2018-03-25 20:53:05 +0800

    :bug: 修复 computed找不到this

--- src/computed.js
+++ src/computed.js
@@ -12,7 +12,7 @@ export default function computed(target, key, descriptor) {
             self[computedKey] = {
                 observers: new Set(),
                 preValue: null,
-                newGet,
+                newGet: newGet.bind(this),
                 observer: null,
                 isShouldUpdate: false,
             };
